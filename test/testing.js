d38c3090137529403dbd733526efbf8ffa0c7338
ahhzamm <mdazamkhalid@gmail.com>
2022-05-20 11:04:53 +0800

    azam

---
+++ test/testing.js
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Lab 2</title>
+</head>
+<body>
+
+<h1>This is heading 1</h1>
+<p>This is text of heading 1.</p>
+<a href="https://www.google.com/">Take me to Google</a>
+
+</body>
+</html>
